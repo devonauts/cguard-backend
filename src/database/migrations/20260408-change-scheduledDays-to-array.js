eefee43ea2320eeaabe68bbc425e4eb7900a4ab4
jp123468 <jose.panchi@epn.edu.ec>
2026-04-10 10:47:32 -0500

    arreglon para migra

--- src/database/migrations/20260408-change-scheduledDays-to-array.js
+++ src/database/migrations/20260408-change-scheduledDays-to-array.js
@@ -5,6 +5,13 @@ module.exports = {
     const dialect = queryInterface.sequelize.getDialect();
     const type = dialect === 'mysql' ? Sequelize.JSON : Sequelize.ARRAY(Sequelize.STRING);
 
+    if (dialect === 'mysql') {
+      // Normalize invalid JSON values before changing the column type.
+      await queryInterface.sequelize.query(
+        `UPDATE siteTours SET scheduledDays = '[]' WHERE scheduledDays IS NULL OR scheduledDays = '' OR JSON_VALID(scheduledDays) = 0`,
+      );
+    }
+
     await queryInterface.changeColumn('siteTours', 'scheduledDays', {
       type,
       allowNull: true,
